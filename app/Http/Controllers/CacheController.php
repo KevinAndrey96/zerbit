0116138817865697daf5f52169b9126c2889fd54
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2021-08-16 02:04:03 -0500

    updated cache

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\ChEvolution;
 use App\Models\ChPsychotherapeuticAssessment;
 use App\Models\ClinicalHistory;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Artisan;
 use Barryvdh\DomPDF\Facade as PDF;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Spatie\Permission\PermissionRegistrar;
 use Symfony\Component\Process\Process;
 
 /**
@@ -22,6 +26,10 @@ class CacheController extends Controller
 
         //$process->run();
         Artisan::call("optimize");
+        app('cache')
+            ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
+            ->forget(config('permission.cache.key'));
+        app()->make(PermissionRegistrar::class)->forgetCachedPermissions();
         /*$evolutions = ChEvolution::all();
         foreach ($evolutions as $evolution) {
             $base64_str = substr($evolution->signature, strpos($evolution->signature, ",")+1);
